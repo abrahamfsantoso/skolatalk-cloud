f2262c4d5099ffb8e1f607bba22072297381302e
Abraham F. Santoso <dev.services@woven.sg>
2025-10-01 20:44:27 +0700

    3.0

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.get('/health', (req, res) => {
 // Main endpoint
 app.get('/', (req, res) => {
   res.json({
-    message: 'Hello from ECS Fargate! v2.0',
+    message: 'Hello from ECS Fargate! v3.0',
     container: process.env.HOSTNAME,
     region: process.env.AWS_REGION || 'not-set'
   });
